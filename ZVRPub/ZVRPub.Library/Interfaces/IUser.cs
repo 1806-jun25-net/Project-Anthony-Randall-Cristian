3354b5f9b7ecfdce8fda31118fc1e81d30fc16e5
Cristian J. Rosario <cris0u0rosario@gmail.com>
2018-07-23 12:12:18 -0400

    some interfces

---
+++ ZVRPub/ZVRPub.Library/Interfaces/IUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZVRPub.Library.Interfaces
+{
+    interface IUser
+    {
+         int UserId { get; set; }
+         string Username { get; set; }
+         string FirstName { get; set; }
+         string LastName { get; set; }
+         DateTime DateOfBirth { get; set; }
+         string UserAddress { get; set; }
+         string PhoneNumber { get; set; }
+         string Email { get; set; }
+         string UserPic { get; set; }
+         
+    }
+}
---
+++ ZVRPub/ZVRPub.Library/Interfaces/IUserLoginInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZVRPub.Library.Interfaces
+{
+    interface IUserLoginInfo
+    {
+        string UserName{ get; set; }
+        string Password { get; set; }
+    }
+}
