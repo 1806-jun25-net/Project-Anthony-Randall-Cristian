95d815ec0a465b7a07e17a160aca2b6c3cfe3b0b
ajzoell <ajzoellner@gmail.com>
2018-07-27 11:35:27 -0400

    working on trying to integrate unit testing. May need to move this work to the API.

---
+++ ZVRPub/ZVRPub.Test/UserControllerTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ZVRPub.MVCFrontEnd.Models;
+
+namespace ZVRPub.Test
+{
+    public class UserControllerTesting
+    {
+        [Fact]
+        public async Task UserIndexAsyncShouldReturnAllUsers()
+        {
+            var mockRepo = new 
+        }
+
+        public List<User>
+    }
+}
